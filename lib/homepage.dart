aab733ec1c306384682f3e32d96acf20867a076c
rupak9090 <rupaksahoo9@gmail.com>
2024-07-28 17:06:07 +0530

    first commit

---
+++ lib/homepage.dart
@@ -0,0 +1,72 @@
+import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+class Homepage extends StatefulWidget {
+  @override
+  State<Homepage> createState() => _HomepageState();
+}
+
+class _HomepageState extends State<Homepage> {
+  SharedPreferences? prefs;
+
+  int visitTime = 0;
+
+  setvisit() async {
+    setState(() {
+      visitTime++;
+    });
+    await prefs!.setInt('uservisit', visitTime);
+  }
+
+  @override
+  void initState() {
+    getstate();
+  }
+
+  getstate() async {
+    prefs = await SharedPreferences.getInstance();
+    setState(() {
+      visitTime = prefs!.getInt('uservisit') ?? 0;
+    });
+    setvisit();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: (visitTime % 5 == 0) ? Colors.blue : Colors.white,
+      body: Center(
+          child: Text(
+        "You have $visitTime time Visit  ",
+        style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
+      )),
+    );
+  }
+}
+
+
+/*
+   SharedPreferences? prefs;
+
+  int visitTime = 0;
+
+  setvisit() async {
+    setState(() {
+      visitTime++;
+    });
+    await prefs!.setInt('uservisit', visitTime);
+  }
+
+  @override
+  void initState() {
+    getstate();
+  }
+
+  getstate() async {
+    prefs = await SharedPreferences.getInstance();
+    setState(() {
+      visitTime = prefs!.getInt('uservisit') ?? 0;
+    });
+    setvisit();
+  }
+*/
---
+++ lib/main.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:user_rept_ui/homepage.dart';
+
+void main() {
+  runApp(myApp());
+}
+
+class myApp extends StatelessWidget {
+  const myApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Homepage(),
+    );
+  }
+}
